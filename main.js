80f8835ea7dcb54ae95abaa1267591960a862872
Deep <45520903+deepmeena70@users.noreply.github.com>
2023-01-29 16:42:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,38 +4,41 @@
   const body = document.querySelector('body');
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-  const numberOfParticles = 615;
+  const numberOfParticles = 865;
   let particles = [];
   let raf;
 
   class Particle {
     constructor() {
       this.x = 200 * Math.random();
-      this.y = 30;
-      this.size = 3;
-      this.color = 'hsl(150deg 100% 60%)';
+      this.y = 45;
+      this.size = 0;
       this.hslDeg = 50 * Math.random();
       this.deg = 0;
+      this.hslDeg = 150;
+      this.hslX = 100;
+      this.hslY = 60;
       this.translateX = canvas.width / 2;
       this.translateY = canvas.height / 2;
+      this.globalAlpha = 1;
     }
     rotate() {
       this.deg += 0.5;
     }
     scatter() {
-      this.x -= 1 ;
-      this.y -= 1/2;
+      this.x -= 1 / 2;
+      this.y -= 1 / 3;
     }
     draw() {
       ctx.save();
-      ctx.globalAlpha = 1;
+      ctx.globalAlpha = this.globalAlpha;
       ctx.translate(this.translateX, this.translateY);
       ctx.rotate((this.deg * Math.PI) / 180);
       ctx.shadowColor = 'lightgrey';
       ctx.shadowBlur = 3;
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.fillStyle = this.color;
+      ctx.fillStyle = `hsl(${this.hslDeg}deg ${this.hslX}% ${this.hslY}%)`;
       ctx.fill();
       ctx.closePath();
       ctx.restore();
@@ -56,7 +59,7 @@
   };
 
   const clear = () => {
-    ctx.globalAlpha = 0.18;
+    ctx.globalAlpha = 0.185;
     ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   };
@@ -65,15 +68,16 @@
   const animate = () => {
     clear();
     for (let i = 0; i < particles.length; i++) {
-      if (particles[i].deg > 300) {
-        particles[i].deg = i * 100;
+      if (particles[i].deg > 360) {
+        particles[i].deg = i/10 ;
       }
-      particles[i].deg -= i / 200;
-      particles[i].size = (i/100);
+      particles[i].deg -= i / 270;
+      particles[i].size = i / 400;
       particles[i].draw();
       particles[i].rotate();
       particles[i].scatter();
     }
+    cancelAnimationFrame(raf);
     raf = requestAnimationFrame(animate);
   };
 
@@ -99,9 +103,8 @@
       p.translateX = X;
       p.translateY = Y;
       p.x = 200 * Math.random();
-      p.y = 30;
-      
-      p.draw();
+      p.y = 45;
+      p.hslDeg = X + Y / 2;
     });
   };
 
